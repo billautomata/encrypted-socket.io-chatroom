0fdb4cd991de61e933096fde8e25f4ff50525605
billautomata <bill@evident.io>
2016-02-27 17:36:36 -0800

    v0

--- crypto-proxy.js
+++ crypto-proxy.js
@@ -5,8 +5,31 @@ var client_io = require('socket.io')(http);
 
 var crypto = require('crypto')
 
+// registers web clients
+// generates keys for them
+
+// server sends an encrypted message
+// in the form of
+// {
+//   from: id,
+//   to: id,
+//   msg: aes256 encrypted message, with the key being the shared secret of the two keys
+// }
+//
+// if from_id is in keys array and to_id is in clients array
+// get private key from clients array and publickey from keys array
+// compute shared secret and decrypt message
+// send decrypted message to web client[to_id]
+
+// web client sends a message - broadcast
+//   get the keypair for the sender
+//    for each publickey in keys
+//      compute the shared secret
+//      encrypt the message
+//      send it to the server
 
 var clients = []
+var keys = []
 
 // Connect to server
 var server_io = require('socket.io-client');
@@ -40,7 +63,7 @@ client_io.on('connection', function(client){
     server_socket.emit('remove_keypair', {
       id: client.id
     })
-    
+
     clients = clients.filter(function(c){return c !== client})
     console.log('socket disconnected', clients.length, 'clients remaining.')
   })
